91fd8926a9921699b6a64a1b0b99019f099a35cd
bezalel6 <bezalel3250@gmail.com>
2023-06-29 22:24:44 +0300

    got rid of notifs

--- src/App.tsx
+++ src/App.tsx
@@ -24,84 +24,16 @@ const auth = firebase.auth();
 
 const App: FC = () => {
   const [user, setUser] = React.useState<firebase.User | null>(null);
-  const [msgs, setMessages] = React.useState(new Array<string>());
 
   React.useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setUser(user);
     });
-    initNotifs();
     return unsubscribe;
   }, []);
 
-  function initNotifs() {
-    if ("serviceWorker" in navigator && "PushManager" in window) {
-      window.addEventListener("load", () => {
-        navigator.serviceWorker
-          .register("/firebase-messaging-sw.js")
-          .then((registration) => {
-            console.log(
-              "Service Worker registered with scope: ",
-              registration.scope
-            );
-            askForNotificationPermission()
-              .then(() => {
-                console.log("Notification permission granted.");
-                granted();
-              })
-              .catch((error) => console.error(error));
-          })
-          .catch((error) => {
-            console.error("Service Worker registration failed: ", error);
-          });
-      });
-
-      const askForNotificationPermission = async () => {
-        const permission = await Notification.requestPermission();
-        if (permission !== "granted") {
-          throw new Error("Permission not granted for Notification");
-        }
-        // You can subscribe to push notifications here if you wish
-      };
-      async function granted() {
-        // Retrieve Firebase Messaging object.
-        const messaging = firebase.messaging();
-        messaging
-          .getToken({
-            vapidKey:
-              "BFWFPkniJjk54Px7g27LVRS7ih-IzwgDNX1KlnIDjIHhdeu-OM301aNjLPjhGeNucA8GgjhA8-LsY0XKMUExspo",
-          })
-          .then((currentToken) => {
-            if (currentToken) {
-              console.log("FCM registration token: ", currentToken);
-              const lll = (e) => {
-                setMessages((m) => [...m, e.notification.body]);
-
-                console.log("!!!!!", e);
-                alert(JSON.stringify(e));
-              };
-              messaging.onMessage(lll);
-            } else {
-              console.log(
-                "No registration token available. Request permission to generate one."
-              );
-            }
-          })
-          .catch((err) => {
-            console.log("An error occurred while retrieving token. ", err);
-          });
-      }
-      // Trigger the notification permission request
-    }
-  }
-
   return (
-    <div className="App">
-      {msgs.map((m) => (
-        <h3>{m}</h3>
-      ))}
-      {user ? <LoggedIn user={user} /> : <SignIn />}
-    </div>
+    <div className="App">{user ? <LoggedIn user={user} /> : <SignIn />}</div>
   );
 };
 
