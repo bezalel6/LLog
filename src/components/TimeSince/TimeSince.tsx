492df3d4f235a1027d9cfe7b26b1db0779f7acf8
bezalel6 <bezalel3250@gmail.com>
2023-08-12 10:50:30 +0300

    formatted time since

--- src/components/TimeSince/TimeSince.tsx
+++ src/components/TimeSince/TimeSince.tsx
@@ -6,6 +6,16 @@ function TimeSince({ date }) {
     const duration = moment.duration(moment().diff(moment(date)));
     const hours = Math.floor(duration.asHours());
     const minutes = Math.floor(duration.asMinutes()) - hours * 60;
+    if (hours > 12) {
+      return moment(date).fromNow();
+    }
+    if (!hours && !minutes) {
+      return "Now";
+    }
+    if (!minutes) {
+      return `${hours} hours ago`;
+    }
+    if (!hours) return `${minutes} minutes ago`;
     return `${hours} hours and ${minutes} minutes ago`;
   };
 
